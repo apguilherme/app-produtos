4fa8c3726163ba1b483d902b4e432a0fe4d961b0
Guilherme <guilherme_070@hotmail.com>
2020-11-23 19:33:32 -0300

    improved products feed/list to show cards

--- src/components/Produto.js
+++ src/components/Produto.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Text, View, Image, StyleSheet } from 'react-native'
+import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native'
+
+import moment from 'moment'
+import 'moment/locale/pt'
 
 export default function Produto({ produto }) {
 
@@ -11,23 +14,109 @@ export default function Produto({ produto }) {
     }*/
 
     return (
-        <View >
-            <Text>Produto: {produto.nomeProduto}</Text>
-            <Text>Descrição: {produto.descricao}</Text>
-            <Text>Qualidade: {produto.qualidade}</Text>
-            <Text>Preço unitário: {produto.valorUnitario}</Text>
-            <Text>Endereco: {produto.enderecoProduto}</Text>
-            <Text>Vendedor: {produto.id_usuarioVendedor.nomeUser}</Text>
-            <Text>Categoria: {produto.id_categoriaProduto.nomeCategoriaProduto}</Text>
-            <Text>Quantidade estoque: {produto.quantidadeEstoque}</Text>
-            <Image style={styles.imagem} source={{ uri: `${produto.linkImagem}` }} />
+        <View style={styles.card}>
+
+            <View style={styles.group}>
+
+                <Image style={styles.imagemUser} source={{ uri: 'https://digimedia.web.ua.pt/wp-content/uploads/2017/05/default-user-image.png' }} />
+
+                <View>
+                    <Text style={{ fontSize: 16 }}>Anunciado por {produto.id_usuarioVendedor.nomeUser}</Text>
+                    <Text style={{ color: 'gray' }}>{moment(produto.createdAt).calendar()}</Text>
+                    <Text style={{ color: 'gray' }}>Em {produto.enderecoProduto}</Text>
+                </View>
+
+            </View>
+
+            <Text style={styles.divider}></Text>
+
+            <View style={styles.cardContent}>
+
+                <Text style={styles.title}>
+                    {produto.nomeProduto} <Text style={{ color: 'gray' }}> ({produto.qualidade})</Text>
+                </Text>
+
+                <View style={styles.group}>
+
+                    <Image style={styles.imagemProduto} source={{ uri: `${produto.linkImagem}` }} />
+
+                    <View>
+                        <Text style={{fontWeight: 'bold'}}>R$ {parseFloat(produto.valorUnitario).toFixed(2)}</Text>
+                        <Text>{produto.id_categoriaProduto.nomeCategoriaProduto}</Text>
+                        <Text>Em estoque: {produto.quantidadeEstoque}</Text>
+                    </View>
+
+                </View>
+
+                <Text style={{ color: 'gray' }}>Descrição: {produto.descricao}</Text>
+
+            </View>
+
+            <Text style={styles.divider}></Text>
+
+            <View style={styles.group}>
+
+                <TouchableOpacity onPress={() => console.warn('Detalhes em breve...')} style={styles.btn}>
+                    <Text style={styles.btnText}>Detalhes</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => console.warn('Carrinho em breve...')} style={styles.btn}>
+                    <Text style={styles.btnText}>Adicionar ao carrinho</Text>
+                </TouchableOpacity>
+
+            </View>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    imagem: {
-        width: 50,
-        height: 50,
+    imagemProduto: {
+        width: 120,
+        height: 120,
+    },
+    imagemUser: {
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        borderWidth: 1,
+        borderColor: '#D9DDDC',
+    },
+    card: {
+        borderRadius: 10,
+        elevation: 1,
+        marginHorizontal: 6,
+        marginVertical: 8,
+        backgroundColor: '#fff',
+    },
+    cardContent: {
+        marginLeft: 15,
+        marginRight: 15,
+    },
+    divider: {
+        borderBottomColor: '#D9DDDC',
+        borderBottomWidth: 1,
+        marginLeft: 15,
+        marginRight: 15,
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    group: {
+        flexDirection: 'row',
+        alignItems: "center",
+        justifyContent: "space-around",
+        padding: 5,
+        margin: 5,
+    },
+    btn: {
+        borderRadius: 5,
+        backgroundColor: '#2196F3',
+        padding: 10,
+        marginBottom: 10,
+    },
+    btnText: {
+        color: '#fff',
+    },
+    title: {
+        fontSize: 18,
     },
 })
